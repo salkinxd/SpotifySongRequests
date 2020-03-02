dc121a31cdb30d9b734e5de17dcf34dfd396d1cc
Niklas Wonnebauer <wonnebauer.salkinxd@gmail.com>
2020-03-02 15:43:02 +0100

    Bunch of stuff

--- app.js
+++ app.js
@@ -1,38 +1,55 @@
-const {client_secret, client_id, refresh_token, playlist_id, prefix} = require('./config.json');
+const {client_secret, client_id, refresh_token, playlist_id, prefix, BOT_USERNAME, OAUTH_TOKEN, CHANNEL_NAME} = require('./config.json');
 var request = require('request'); // "Request" library
-//var trackURI = "spotify:track:7d8GetOsjbxYnlo6Y9e5Kw";
-var channel = "salkinxd";
-var botUsername = "deepshit";
+var tmi = require('tmi.js');
+const opts = {
+  identity: {
+    username: BOT_USERNAME,
+    password: OAUTH_TOKEN
+  },
+  channels: [
+    CHANNEL_NAME
+  ]
+};
 
+// Create a client with our options
+const client = new tmi.client(opts);
 
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
 
-
-const { ChatClient } = require("dank-twitch-irc");
- 
-let client = new ChatClient();
- 
-client.on("ready", () => console.log("Successfully connected to chat"));
-client.on("close", error => {
-  if (error != null) {
-    console.error("Client closed due to error", error);
-  }
-});
- 
-client.on("message", msg => {
-  
-  if (msg.senderUsername != botUsername && msg.messageText != undefined) {
-    console.log(msg.messageText + " | " + msg.senderUsername);
-    const args = msg.messageText.slice(prefix.length).split(' ');
-    const command = args.shift().toLowerCase();
-    if (command == "sr") {
-      console.log(args);
-      addToPlaylist(args);
-    }
-  }
-}); 
+// Connect to Twitch:
 client.connect();
-client.join(channel);
 
+
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
+
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+  console.log(msg);
+  // Remove whitespace from chat message
+  const args = msg.slice(prefix.length).split(' ');
+  const command = args.shift().toLowerCase();
+  console.log(command)
+  console.log(args)
+  // If the command is known, let's execute it
+  if (command === 'sr') {
+    var arguments = args.join(" ").trim();
+    addToPlaylist(arguments);
+  }
+  if (command === 'srplaylist') {
+    client.say(CHANNEL_NAME, "This is the playlist: https://open.spotify.com/playlist/" + playlist_id)
+    .then((data) => {
+        console.log("Sent Message!");
+    }).catch((err) => {
+        console.error(err);
+    });
+  }
+}
 
 function addToPlaylist(args) {
   var authOptions = {
@@ -50,14 +67,15 @@ function addToPlaylist(args) {
     console.log(response.statusCode);
     console.log(response.statusMessage);
     if (!error && response.statusCode === 200) {
-      args = encodeURIComponent(args.join(" ").trim());
+      
       var findSongOptions = {
-        url: "https://api.spotify.com/v1/search?q=" + args + "&type=track&limit=1",
+        url: "https://api.spotify.com/v1/search?q=" + encodeURIComponent(args) + "&type=track&limit=1",
         headers: { 'Authorization': 'Bearer ' + access_token}
       }
-      request.get(findSongOptions, function(error, response, body) {
+      request.get(findSongOptions, function(error, response, findSongBody) {
         if (!error) {
-          var trackURI = JSON.parse(body).tracks.items[0].uri
+          console.log(JSON.parse(findSongBody).tracks.items[0])
+          var trackURI = JSON.parse(findSongBody).tracks.items[0].uri
           requestURL = "https://api.spotify.com/v1/playlists/" + playlist_id + "/tracks?position=0&uris=" + trackURI;
           var addTrackOptions = {
             url: requestURL,
@@ -65,6 +83,12 @@ function addToPlaylist(args) {
           };
           request.post(addTrackOptions, function(error, response, body) {
             console.log(body);
+            client.say(CHANNEL_NAME, "Added '" + JSON.parse(findSongBody).tracks.items[0].name + " - " + JSON.parse(findSongBody).tracks.items[0].artists[0].name + "' to the Playlist!")
+            .then((data) => {
+                console.log("Sent Message!");
+            }).catch((err) => {
+                console.error(err);
+            });
           })
         } else {
           console.log(error);
