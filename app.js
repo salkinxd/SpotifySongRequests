463aa181d29c4d5bbaa014b4bda4663edfa14667
Niklas Wonnebauer <wonnebauer.salkinxd@gmail.com>
2020-03-12 22:31:03 +0100

    Added timestamp before log and other stuff

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('console-stamp')(console, 'HH:MM:ss');
 var SpotifyWebApi = require('spotify-web-api-node');
 var config = require("./config.json");
 var tmi = require('tmi.js');
@@ -58,27 +59,30 @@ function onMessageHandler(target, context, msg, self) {
 	// Remove whitespace from chat message
 	const args = msg.slice(config.prefix.length).split(' ');
 	const command = args.shift().toLowerCase();
-	console.log("Command: " + command) //Writes command in console
-	console.log("Args: " + args) //Writes arguments in console
+	/*if (command != "sr" || command != "srplaylist" || command != "song" || command != "skip") { return; }*/
+	//console.log(context['display-name'] + "|" + context['mod'] + "| ->", msg)
 	// If the command is known, let's execute it
 	if (command === 'sr') {
+		console.log("Command: ", command, "Args: ", args) //Writes command in console
 		if (args.length > 0) {
 			//If there is a song name behind the command -> add to playlist
-			var arguments = args.join(" ").trim();
+			var arguments = args.filter(e => e != "von").join(" ").trim();
 			addToPlaylist(arguments);
 		} else {
 			//If there are no words after command -> Say in chat how to add songs
-			client.say(config.CHANNEL_NAME, "Mit \"!sr song\" kannst du einen Song anfragen!")
+			client.say(config.CHANNEL_NAME, "Mit \"!sr Song - Künstler\" kannst du einen Song anfragen!")
 				.then((data) => {
-					console.log("Sent Message!");
+					console.log("Sent Message!", data);
 				}).catch((err) => {
 					console.error(err);
 				});
 		}
-
 	}
+
 	//!srplaylist Command
 	if (command === 'srplaylist') {
+		console.log("Command: " + command) //Writes command in console
+		console.log("Args: " + args) //Writes arguments in console
 		//Message the playlist url
 		client.say(config.CHANNEL_NAME, "Das ist die Playlist: https://open.spotify.com/playlist/" + config.playlist_id)
 			.then((data) => {
@@ -89,11 +93,15 @@ function onMessageHandler(target, context, msg, self) {
 	}
 	//!song command
 	if (command === "song") {
+		console.log("Command: " + command) //Writes command in console
+		console.log("Args: " + args) //Writes arguments in console
 		//Check for currently  playing song
 		nowPlaying();
 	}
 	//!skip command
 	if (command === "skip") {
+		console.log("Command: " + command) //Writes command in console
+		console.log("Args: " + args) //Writes arguments in console
 		//Check for mod and usernames
 		if (context['mod'] || context['display-name'] === "Salkinxd" || context['display-name'] === "SpiceTV") {
 			console.log("User is a mod!");
@@ -202,4 +210,4 @@ function skipTrack() {
 		})
 }
 
-setTimeout(refreshToken, 3500000)
+setInterval(refreshToken, 3500000);
